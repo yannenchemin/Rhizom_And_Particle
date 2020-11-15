581bc85b805141d76e63d59f86332f4c56ede88e
yann en chemin <73861577+yannenchemin@users.noreply.github.com>
2020-11-15 22:53:30 +0900

    add red balls

--- sketch.js
+++ sketch.js
@@ -11,17 +11,27 @@
 let systems = [];
 
 function setup() {
-  let text = createP("click to add particle systems");
+  let text = createP("click to add rhizom systems");
   text.position(10, 365);
 
   createCanvas(640, 360);
+  background(0);
+    for(let j = 0; j< 10; j++){
+      stroke(255, this.lifespan);
+      strokeWeight(2);
+      fill(255,0,0);
+      ellipse(random(width),random(height),30);
+ }
 }
 
 function draw() {
-  background(51);
+
   for (let i = 0; i < systems.length; i++) {
     systems[i].addParticle();
     systems[i].run();
+
+  //  systems1[j].;
+
   }
 }
 
